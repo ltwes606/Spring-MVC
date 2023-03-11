361f60a53f0472ea1ff1ee3d8b0afbd001300d4c
ltwes606 <ltwes606@gmail.com>
2023-03-11 22:47:24 +0900

    Front Controller 방식 도입 Version1

---
+++ servlet/src/main/java/hello/servlet/web/frontcoltroller/v1/MemberListControllerV1.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.frontcoltroller.v1;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
